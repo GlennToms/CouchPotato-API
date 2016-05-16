97d17c3fc5ce73f58d098f547e3e0ae0eb58b009
GlennToms <Glenn@Glenntoms.gg>
2016-05-16 21:25:05 +0100

    Update Program.cs

--- CouchPotato/CouchPotato.Sample/Program.cs
+++ CouchPotato/CouchPotato.Sample/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CouchPotato.Model.Movie;
-using Type = CouchPotato.Model.Logging.Type;
+
 
 namespace CouchPotato.Sample
 {
@@ -13,14 +8,13 @@ namespace CouchPotato.Sample
         private static void Main(string[] args)
         {
             //Create a client.
-            const string apiKey = "8fbfe59b9aee44c1ac42524ca8953be1";
-            const string url = "http://Server01:6886"; //Replace by your CouchPotato location Hostname or IP
+            const string apiKey = "123456789abcdefghijk";
+            const string url = "http://127.0.0.1:8083"; //Replace by your CouchPotato location Hostname or IP
             var client = new Client(url, apiKey);
 
             var result = client.CouchPotato.IsAvaliable();
-
-            Console.WriteLine();
-            //Console.WriteLine(result.Total);
+            
+            Console.WriteLine(result.ToString());
             Console.ReadLine();
         }
     }
